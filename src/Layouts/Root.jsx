ec911311a4e8b24a1cbf0a240dfae6fad1fae6ff
Al Shiam Nabil <nabil15-4777@diu.edu.bd>
2025-11-09 01:12:13 +0600

    folder stracture done

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Navbar from '../Components/Headers/Navbar';
+import { Outlet } from 'react-router';
+import Footer from '../Components/Footer/Footer';
 
 const Root = () => {
     return (
         <div>
-            root
+         <header>
+        <Navbar></Navbar>
+         </header>
+
+         <main>
+            <Outlet></Outlet>
+         </main>
+
+         <footer>
+            <Footer></Footer>
+         </footer>
         </div>
     );
 };
