0782fd64a1a9eecafa10dfa599cb9cbbb0753d8b
Al Shiam Nabil <nabil15-4777@diu.edu.bd>
2025-11-10 23:15:58 +0600

    update car form setup with modal

--- src/Pages/MyListingPage.jsx
+++ src/Pages/MyListingPage.jsx
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Container from "../Components/Container/Container";
 import { useContextHook } from "../Hooks/useContextHook";
-import { HiH1 } from "react-icons/hi2";
+import UpdateCarPage from "./UpdateCarPage";
+import LoadingComponent from "../Components/LoadingSpinner/LoadingComponent";
 
 const MyListingPage = () => {
   const { user } = useContextHook();
   const [cars, setCars] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [updatedCar,setUpdatedCar]=useState(null)
+  const updateModalRef = useRef(null);
+
   useEffect(() => {
     fetch(`http://localhost:3000/my-cars?email=${user?.email}`)
       .then((res) => res.json())
@@ -17,7 +21,20 @@ const MyListingPage = () => {
       });
   }, [user?.email]);
 
-  console.log(cars);
+  const handleUpdateModal = (id) => {
+    updateModalRef.current.showModal();
+
+    fetch(`http://localhost:3000/cars/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setUpdatedCar(data);
+        setLoading(false);
+      });
+  };
+
+  if (loading) {
+    return <LoadingComponent></LoadingComponent>;
+  }
 
   return (
     <>
@@ -58,13 +75,164 @@ const MyListingPage = () => {
                     >
                       {car?.status}
                     </td>
-                    <td>update / delete</td>
+                    <td className="space-y-1">
+                      <button
+                        onClick={() => handleUpdateModal(car?._id)}
+                        className="btn btn-sm w-[70px] text-secondary"
+                      >
+                        Update
+                      </button>{" "}
+                      <button className="btn btn-sm w-[70px] text-red-500">
+                        Delete
+                      </button>
+                    </td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
         )}
+
+        {/* modal */}
+
+        <dialog ref={updateModalRef} className="modal">
+          <div className="modal-box max-h-[500px] w-11/12 max-w-[800px]">
+           <form
+        
+          className=" "
+        >
+          <h3 className=" font-semibold text-xl ">
+            {" "}
+           Update your car details
+          </h3>
+          <fieldset className="fieldset">
+            <label className="label text-accent text-base font-medium mt-2">
+              Car Name
+            </label>
+            <input
+              type="text"
+              className="input shadow-none bg-gray-100 border-none outline-none "
+              placeholder="Enter Car Name..."
+              defaultValue={updatedCar?.car_name}
+              name="car_name"
+              spellCheck={false}
+            />
+
+            {/* description */}
+
+            <label className="label text-accent text-base font-medium mt-2">
+              Description
+            </label>
+            <textarea
+            defaultValue={updatedCar?.description}
+              name="description"
+              placeholder="Enter Description Here..."
+              className="textarea-lg p-3 text-sm bg-gray-100 border-none outline-none min-h-[200px] resize-none "
+              spellCheck={false}
+            ></textarea>
+
+            {/* dropdown */}
+            <label className="label text-accent text-base font-medium mt-2">
+              Category
+            </label>
+            <select
+              defaultValue={updatedCar?.category}
+              className="select"
+              name="category"
+            >
+              <option value="defaultValue" disabled={true}>
+                Choose your Category
+              </option>
+              <option value="sedan">Sedan</option>
+              <option value="suv">SUV</option>
+              <option value="hatchback">Hatchback</option>
+              <option value="luxury">Luxury</option>
+              <option value="electric">Electric</option>
+            </select>
+
+            {/* rent price */}
+            <label className="label text-accent text-base font-medium mt-2">
+              Rent Price (Per day)
+            </label>
+
+            <input
+            defaultValue={updatedCar?.price_per_day}
+              type="text"
+              className="input shadow-none bg-gray-100 border-none outline-none "
+              placeholder="Enter Rent Price..."
+              name="price_per_day"
+            />
+
+            {/* location */}
+            <label className="label text-accent text-base font-medium mt-2">
+              Location
+            </label>
+
+            <input
+            defaultValue={updatedCar?.location}
+              type="text"
+              className="input shadow-none bg-gray-100 border-none outline-none "
+              placeholder="Enter Location..."
+              name="location"
+              spellCheck={false}
+            />
+
+            {/* image url */}
+
+            <label className="label text-accent text-base font-medium mt-2">
+              PhotoURL
+            </label>
+
+            <input
+            defaultValue={updatedCar?.photo_url}
+              type="text"
+              className="input shadow-none bg-gray-100 border-none outline-none "
+              placeholder="Enter PhotoURL..."
+              name="photo_url"
+              spellCheck={false}
+            />
+
+            {/* provider name */}
+            <label className="label text-accent text-base font-medium mt-2">
+              Provider Name
+            </label>
+
+            <input
+              type="text"
+              className="input shadow-none bg-gray-100 border-none outline-none "
+              value={user?.displayName}
+              name="provider_name"
+              readOnly
+            />
+
+            {/* provider email */}
+            <label className="label text-accent text-base font-medium mt-2">
+              Provider Email
+            </label>
+
+            <input
+              type="text"
+              className="input shadow-none bg-gray-100 border-none outline-none "
+              value={user?.email}
+              name="provider_email"
+              readOnly
+            />
+
+            <button className="btn btn-secondary hover:btn-primary hover:text-secondary outline-none border-none shadow-none mt-4 w-[150px]">
+             Update Car
+            </button>
+          </fieldset>
+        </form>
+            <div className="modal-action">
+              <form method="dialog">
+                {/* if there is a button, it will close the modal */}
+                <button className="btn">Close</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+
+   
       </Container>
     </>
   );
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -8,6 +8,7 @@ import AddCarPage from "../Pages/AddCarPage";
 import PrivateRoute from "../AuthContext/PrivateRoute";
 import MyListingPage from "../Pages/MyListingPage";
 
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -42,6 +43,8 @@ const router = createBrowserRouter([
           </PrivateRoute>
         ),
       },
+      
+     
     ],
   },
 ]);
